442fa6e810854c4e49155309a8999bb23423c973
Christia Neal <c.neal91@gmail.com>
2016-01-19 11:55:51 -0600

    Add initial views and routes

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+	return HttpResponse("You're looking at the results of question%s." % question_id)
+
+def results(requestion, question_id):
+	response = "You're looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
