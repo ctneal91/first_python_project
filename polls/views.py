0e60e51c68407736d6990d0d77cc63b31a242b75
Christia Neal <c.neal91@gmail.com>
2016-01-20 08:32:26 -0600

    Create all view tests for polls:index

--- polls/views.py
+++ polls/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
 #shortcut way for index, detail, and results
 #
 class IndexView(generic.ListView):
+	
 	template_name = 'polls/index.html'
 	context_object_name = 'latest_question_list'
 	def get_queryset(self):
-		"""Return the last five published questions."""
-		return Question.objects.order_by('-pub_date')[:5]
+		"""
+		Return the last five published questions (not including those to be published in the future).
+		"""
+		return Question.objects.filter(
+			pub_date__lte=timezone.now()
+		).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Question
