8d3c7033a6d387904f524559225a2854b6142d75
Christia Neal <c.neal91@gmail.com>
2016-01-20 05:28:37 -0600

    Add form to detail page and add logic to vote view

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 
-from django.http import HttpResponse
-
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -26,4 +26,18 @@ def results(requestion, question_id):
 	return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist)
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+	return HttpResponseRedirect(reverse('poll:results', args=(question.id,)))
+
+
+
