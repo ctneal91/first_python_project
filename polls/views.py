dceab34bb0dc1800507ece105f30f586a07b283e
Christia Neal <c.neal91@gmail.com>
2016-01-20 06:01:12 -0600

    Allow user to vote and immediately see results in poll

--- polls/views.py
+++ polls/views.py
@@ -21,15 +21,15 @@ def detail(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/detail.html', {'question': question})
 
-def results(requestion, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+def results(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question })
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
-	except (KeyError, Choice.DoesNotExist)
+	except (KeyError, Choice.DoesNotExist):
 		return render(request, 'polls/detail.html', {
 			'question': question,
 			'error_message': "You didn't select a choice.",
@@ -37,7 +37,8 @@ def vote(request, question_id):
 	else:
 		selected_choice.votes += 1
 		selected_choice.save()
-	return HttpResponseRedirect(reverse('poll:results', args=(question.id,)))
+	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 
 
 
